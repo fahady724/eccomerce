8cbe0d5a2f2553a8ff1a617847d22ecfd7e7a2b6
Fahad Younus <fahadyounus129@gmail.com>
2026-08-09 12:00:16 +0600

    Update admin credentials in createDefaultAdmin method

--- src/Main.java
+++ src/Main.java
@@ -33,8 +33,8 @@ public class Main {
     private static void createDefaultAdmin() {
         try {
             AuthService.addAdmin(
-                    "admin",
-                    "admin01",
+                    "fahad",
+                    "fahad01",
                     "fahad@email.com",
                     "01972742013",
                     "Dhaka"
